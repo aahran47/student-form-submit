3cb9838c0d8a979ad68ab96056dc9cdca157d201
aahran47 <parvezrimon55@gmail.com>
2023-12-25 22:13:35 +0600

    php fundamental kichu add kora holo php.php file a

---
+++ php.php
@@ -0,0 +1,732 @@
+
+
+
+## Student Login Form 
+
+<img src="pro.jpg">
+
+<?php 
+    // $name = "samim";    
+    // $marks = 80;
+    // $gen = " vai";
+    // switch($name){
+    //     case 'Amina':           
+    //     case 'Ayesha':           
+    //     case 'Alysha':           
+    //     case 'Ety':
+    //         $gen = ' apu';
+    //         break;
+    // }
+    // if( $name == "Amina" OR $name == "Ayesha" OR $name == "Alysha" OR $name == "Ety"){
+    //     $gen = " apa";
+    // }
+
+
+    // if( $marks >= 0 && $marks <= 32 ){
+    //     echo "<span style=\"color: red;\">". $name. $gen. " apni fail korchen. </span>"; 
+    // }else if( $marks >= 33 && $marks <= 39){
+    //     echo $name . $gen . " apni kono mote pas korchen";
+    // }else if( $marks >= 40 && $marks <= 49){
+    //     echo $name . $gen . " apni jai korchen ate motei hobe dora jai";
+    // }else if( $marks >= 50 && $marks <= 59){
+    //     echo $name . $gen ." apni jemon pass korchen ate piyon ar kaj paven";
+    // }else if( $marks >= 60 && $marks <= 69){
+    //     echo $name . $gen . " apni pass korchen aro valo korte hobe";
+    // }else if( $marks >= 70 && $marks <= 79){
+    //     echo $name .  $gen . " apni aktu holei golden peten";
+    // }else if ($marks >= 80 && $marks <= 100){
+    //     echo $name . $gen . " apni A+ paichen";
+    // }
+
+
+    // $amina = 1;
+
+    // switch($amina){
+    //     case 3 : 
+    //         echo "Apni amake biye korben";
+    //         break;
+    //     case 5 : 
+    //         echo "Apni amake kiss korben";
+    //         break;
+    //     case 8 : 
+    //          echo "Apni amake ador korben";
+    //         break;
+    //     case 10 : 
+    //         echo "Apni amake kamor diben";
+    //         break;
+    //     default: 
+    //         echo "Apni amake miss korchen jani";
+    //         break;
+    // }
+
+        // Data type 
+
+        // 1. String
+        // 2. Integer 
+        // 3. Double/Float
+        // 4. Boolean
+        // 5. Array
+        // 6. Object
+        // 7. NULL
+        // 8. Resource
+        
+        // Operator
+
+        // 1. Arithmetic Operator - + - * / %
+        // 2. Assinment Oparator  - =
+        // 3. Comparative Oparator - < > <= >= == === !=
+        // 4. Increment/ Decrement Oparator - ++ --
+        // 5. Logical Oparator - AND OR NOT && || !
+        // 6. String Oparator - =. .=
+        // 7. Array Oparator - . .
+        // 8. Conditional assinment Oparator - if else elseif 
+
+        // single equel assinment korar kaje lage
+        // double equel compaire kore thake tader value ke 
+        // thripol equel mane holo value o data type compaire kore thake
+
+        // $ami = "120";
+        // $tumi = "120";
+
+        // if( $ami === $tumi ){
+        //     echo "Ami jani tumi ke";
+        // }else{
+        //     echo "Tumi jano ami ke";
+        // }
+
+        // while , do while , for, foreach
+
+
+        // $i = 1;
+
+        // while($i <= 10 ){
+        //     echo "kemon acho tumi priyetro $i <br>";
+        //     $i++;
+        // }
+
+        // $h = 100;
+
+        // while( $h >= 89){
+        //     echo "I Love you $h <br>";
+        //     $h--;
+
+        // }
+        
+        // $k = 200;
+        // $p = 1;
+        
+        
+
+        // while($k < 220){
+        //     if( $p % 2 == 0 ){
+        //         echo "$p Even Number <hr>";
+        //     }else{
+        //         echo "$p Odd Number <hr>";
+        //     }
+        //     $k++;
+        //     $p++;
+        // }
+
+
+        // $g = 50;
+
+        // do{
+        //     echo "value = $g <hr>";
+        //     $g--;
+
+        // }while( $g > 40);
+
+      
+
+        // for($r = 0; $r < 10; $r++){
+        //     echo "php developer <br>";
+        // }
+
+
+       
+
+
+    //    $g = 1;
+
+    //    while( $g <= 10 ){
+    //      echo "3 x $g = " . ($g * 3) . "<br>";
+
+    //      $g++;
+    //    }
+        
+    //    for($a = 1; $a <= 100; $a++){
+
+    //     if( $a % 3 == 0 || $a % 4 == 0 ){
+    //         echo $a . "<br>";
+    //     }
+
+    //    }
+
+    // $w = 1000;
+
+    // while( $w >= 500){
+    //     if( $w % 3 == 0 && $w % 4 == 0){
+    //         echo $w ."<br>";
+    //     }
+
+    //     $w--;
+
+    // }
+
+
+    // for( $ety = 1; $ety < 500; $ety = $ety + 3) {
+    //     echo $ety . "<br>";
+    //     if( $ety % 11 == 0){
+    //         break;   
+           
+    //     }
+
+    // }
+   
+
+    // for($amina = 1; $amina <= 10; $amina++){
+    //     echo "2 x $amina = ". $amina * 2 ."<br>";
+
+    // }
+    // echo "<hr>";
+    // for($amina = 1; $amina <= 10; $amina++){
+    //     echo "3 x $amina = ". $amina * 3 ."<br>";
+
+    // }
+    // echo "<hr>";
+    
+    // for($amina = 1; $amina <= 10; $amina++){
+    //     echo "4 x $amina = ". $amina * 4 ."<br>";
+
+    // }
+    // echo "<hr>";
+    
+    // for($amina = 1; $amina <= 10; $amina++){
+    //     echo "5 x $amina = ". $amina * 5 ."<br>";
+
+    // }
+
+
+    // $ami = "mrs. amina akter (ety)";
+
+    // echo strtoupper($ami);
+
+    // $ety = "I Love you";
+
+    // echo ucfirst($ety);
+
+    // $errr = "Ami kothai jabo janina. koto dur jabo janin, ken jabo janina.";
+
+    // echo str_replace( 'jabo','padi', $errr);
+
+    
+    // function name(parameter){
+
+    // }
+
+    // name(argument);
+
+
+
+    // function shamim_amina($name = "shamim", $job ="php devs"){
+
+    //     echo "My name $name and I am $job.";
+    // }
+
+    // shamim_amina('Parvez', 'phython devs');
+
+
+    // function pvs($hot){
+    //     return "He is a $hot person";
+    // }
+
+    // echo pvs('bad');
+
+    // function jomno( string $name , int $birth_year ){
+
+    //     $age = date('Y') - $birth_year;
+    //     echo "Assalamalaikum $name, You are $age years old.";
+    // }
+
+
+    // jomno('Amina', 1996);
+ 
+    // function borolekha($har, $rong = '#20bf6b', $bgrong = '#fed330'){
+    //     echo "<span style=\" text-transform:uppercase; color: $rong; background-color: $bgrong;\">" . $har . "</span>";
+    // }
+
+
+    // borolekha('amina ami tumake anek valobashi', '#d1d8e0', '#a55eea');
+ 
+    // function amarAmina($ety, $tag= 'h1', $col = '#f7b731', $ali = 'center', $fnd = 'impact') {
+    //     echo "<$tag style=\" color: $col; text-align: $ali; font-family: $fnd;\">" . $ety . "</$tag>";
+    // }
+
+
+    // amarAmina('I Love you jan miss you kolija ta', 'h3', '#f7b731', 'right', 'Brush Script MT');
+
+
+    // function gradepoint($tt){
+    //     $gpa = '';
+    //     $grande = '';
+    //     if($tt >= 0 && $tt <= 32){
+    //         $gpa = '0';
+    //         $grande = 'F';
+    //     }elseif($tt >= 33 && $tt <= 39){
+    //         $gpa = '1';
+    //         $grande = 'E';
+    //     }elseif($tt >= 40 && $tt <= 49){
+    //         $gpa = '2';
+    //         $grande = 'D';
+    //     }elseif($tt >= 50 && $tt <= 59){
+    //         $gpa = '3';
+    //         $grande = 'C';
+    //     }elseif($tt >= 60 && $tt <= 69){
+    //         $gpa = '3.5';
+    //         $grande = 'B';
+    //     }elseif($tt >= 70 && $tt <= 79){
+    //         $gpa = '4';
+    //         $grande = 'A-';
+    //     }elseif($tt >= 80 && $tt <= 100){
+    //         $gpa = '5';
+    //         $grande = 'A+';
+    //     }
+
+
+    //     echo "GPA = $gpa AND GRAND = $grande";
+
+    // }
+
+
+    // gradepoint(45);
+
+
+    // function detectorAge($age){
+    //     if( $age <= 100 && $age >= 50){
+    //         echo "$age !! You are old!!";
+    //     }elseif( $age <= 49 && $age >= 30){
+    //         echo "$age You are adult!!";
+    //     }elseif( $age <= 29 && $age >= 20){
+    //         echo "$age You are young!!";
+    //     }elseif( $age <= 19 && $age >= 10){
+    //         echo "$age You are boy!!";
+    //     }elseif( $age <= 1 && $age >= 9){
+    //         echo "$age You are child!!";
+    //     }else{
+    //         echo "Your are a alien!!!!";
+    //     }       
+
+    // }
+
+    // detectorAge(5000);
+
+    // function mapo($l = NULL,  $w = NULL, $t= 'r'){
+
+    //    if($t == 'r'){
+
+    //     $are = $l * $w;
+    //     echo "Area = $are";
+
+    //    }elseif($t == 's'){
+
+    //     $are = $l * $l;
+    //     echo "Area = $are";
+         
+    //    }elseif( $t == 'c'){
+        
+    //     $are = 3.1416 * ($l * $l);
+    //     echo "Area = $are";
+
+    //    }
+
+    // }
+
+    // // mapo(200, null, 's');
+
+    // function bmi($m, $h){
+
+    //     $height = $h * 0.3048;
+    //     $bmi = $m / ($height * $height);  
+
+    //     echo "BMI = $bmi";
+
+    // }
+
+    // bmi(50, 5.8);
+
+
+
+    // function headingText($txt , $color= 'lal'){
+
+    //     $text_color = '';
+    //     switch($color){
+
+    //         case 'lal':
+    //             $text_color = 'red';
+    //             break;
+    //         case 'nil':
+    //             $text_color = 'blue';
+    //             break;
+    //         case 'sobuj':
+    //             $text_color = 'green';
+    //             break;
+    //         case 'holud':
+    //             $text_color = 'yellow';
+    //             break;
+    //         default:
+    //             $text_color = 'yellow';
+    //             break;
+                
+                
+    //     }
+    //     echo "<h1 style= 'color:". $text_color .";'> $txt </h1>";
+
+    // }
+
+    // headingText('Samim Hossain','nil');
+
+
+    // function currencyEvalution($taka, $converted = 'u'){
+
+    //     $usd = 110.014;
+    //     $cd = 82.21;
+    //     $pound = 139.61;
+
+    //     if($converted == 'u'){
+    //         $ut = $taka*$usd;
+    //         echo "USD = $ut TAKA";
+    //     }elseif($converted == 'c'){
+    //         $ct = $taka*$cd;
+    //         echo "CANADIAN USD = $ct TAKA";
+    //     }elseif($converted == 'p'){
+    //         $pt = $taka*$pound;
+    //         echo "POUND = $pt TAKA";
+    //     }
+
+    // }
+
+
+    // currencyEvalution(2, 'p');
+
+
+    // function ttt($taka, $convert='u'){
+        
+    //     $usd = 110.014;
+    //     $cd = 82.21;
+    //     $pound = 139.61;
+
+    //     $ggg = '';
+    //     switch($convert){
+    //         case 'u':
+    //             $ggg = "USD ". $taka*$usd ." TAKA";
+    //             break;
+    //         case 'c':
+    //             $ggg = "CANADIAN USD ". $taka*$cd." TAKA";
+    //             break;
+    //         case 'p':
+    //             $ggg = "POUND ". $taka*$pound." TAKA";
+    //             break;
+    //         default:
+    //             $ggg= "USD ". $taka*$usd." TAKA";
+    //             break;
+
+    //     }
+
+    //     echo $ggg;
+         
+    // }
+
+
+    // ttt(4, 'p');
+
+
+    // function birthday($year){
+
+    //     $age = date('Y') - $year;
+
+    //     echo "Assalamalaikum, Your are $age years old.";
+
+    // }
+
+    // birthday(1995);
+
+
+    // <bdo dir="rtl">
+    // This text will go right-to-left.
+    // </bdo> 
+
+    // ✔	Indexed array 
+    // ✔	Associative array 
+    // ✔	Multi-dimensional array
+    
+
+
+    // $amina = ['shamim', 'alysha', 'ayehsa', 'arafat', 'ety',143, null, true];
+
+
+    // $love = [
+    //    'mother'     => 'khida lagche tumar',
+    //    'father'     => 'koto taka lagche',
+    //    'girlfriend' => 'tumake cara bachbona ami',
+    //    'son'        => 'baba tumar gorita amake diba',
+    //    'daughter'   => 'maa tumake dekhte amar moto lagche',
+    //    'wife'       => 'tumi amar deya t-shirt ta poro na keno',
+    //    'husband'    => 'tumar chul ta kintu khub sundor'
+
+    // ];
+    
+    
+    
+    // echo "<pre>";
+    // print_r($love);
+    // echo "</pre>";
+
+
+    // $subjects = ['bangla','english','math','religion','science','sociel','computer','accounting'];
+
+
+    // foreach( $subjects as $allhere){
+        
+    //     echo "This is my subject $allhere. <br>";
+
+    // }
+    
+
+
+    // $students = [
+    //     '01' => [
+    //         'name'      => 'amina',
+    //         'roll'      => '01',
+    //         'class'     => 'five',
+    //         'age'       => 10,
+    //         'cell'      =>  '017153525458',
+    //         'sub' => [
+    //             'ban'   => 10,
+    //             'eng'   => 12,
+    //             'mat'   => 15,
+    //             'sc'    => 13,
+    //             'ss'    => 10
+    //         ]
+    //         ],
+    //     '02' => [
+    //         'name'      => 'samim',
+    //         'roll'      => '01',
+    //         'class'     => 'five',
+    //         'age'       => 11,
+    //         'cell'      =>  '01718585468',
+    //         'sub' => [
+    //             'ban'   => 15,
+    //             'eng'   => 10,
+    //             'mat'   => 11,
+    //             'sc'    => 14,
+    //             'ss'    => 12
+    //         ]
+    //     ]
+    // ];
+
+
+    // echo $students['01']['name'] . '<br>';   
+    // echo $students['01']['class'] . '<br>';
+    // echo $students['01']['sub']['ss'];
+      
+
+
+//     $fuck = ['hard', 'soft','minimal','lubricant', 'spread_semen_on_face','forplay', 'licking', 'dicksucking', 'pussysucking','lipsbiting', 'breast_niple_sucking'];
+
+//     // array_pop($fuck);/// delete from end 
+//     // array_push($fuck, 'oralsex');/// create from end 
+//     // array_push($fuck, 'portablesex');/// create from end 
+//     // array_push($fuck, 'portablesex');/// create from end 
+//     // array_push($fuck, 'organismsex'); /// create from end 
+//     // array_push($fuck, 'organismsex'); /// create from end 
+//     // // array_shift($fuck); /// delete from start 
+//     // array_unshift($fuck, 'breastsex');////create from start 
+
+
+//     // $sex = array_reverse($fuck);
+//     // $sex = array_unique($fuck);
+//     // $only_sucking = array_splice($fuck, 5, 6);
+
+//    echo '<pre>';
+//    print_r($fuck);
+//    echo '<pre>';
+
+//    echo '<pre>';
+//    print_r($only_sucking);
+//    echo '<pre>';
+
+
+
+//    echo '<pre>';
+//    print_r($sex);
+//    echo '<pre>';
+
+
+   
+//    echo '<pre>';
+//    print_r($sex);
+//    echo '<pre>';
+
+
+    // $name = 'Samim hossain';
+    // $pass = '123abc';
+
+    // echo password_hash($pass, PASSWORD_DEFAULT);
+
+
+// echo $fuck =  str_shuffle('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890');
+
+// echo "<br>";
+
+// echo substr($fuck, 10, 5);
+
+// date_default_timezone_set('asia/dhaka');
+
+// // echo date('F d, Y g:i:s a', 254813855);
+
+
+// mail('ameshahabibialbi@gmail.com','Test','Assalmalaikum');
+
+
+//**** */ string type ar data ke explode korte array te convert hoye thake
+
+//***** */ array type ar data ke implode kore string te convert kora hoye thake
+
+/// explode()
+
+
+// $amina = "I love you, I want to marry you, I want to kiss you, I want to make you happy";
+
+// $ety = explode(' ', $amina);
+
+
+// echo "<pre>";
+// print_r($ety);
+
+
+////implode()
+
+
+// $bou = ['sundor','sweet_bou','caring','adorable','funny','long_hair','best_cooking','best_designer','best_inventor','recycling_unuse_thing'];
+
+// echo "<pre>";
+// print_r($bou);
+
+// $porita = implode(' Amina ', $bou);
+
+// echo $porita;
+
+//include   ---- jotobar load kora hobe toto bar pave
+//include_once --akbar load korlei holo
+
+//require ---- jotobar load kora hobe toto bar pave
+//require_once --akbar load korlei holo
+
+
+
+// if( file_exists('about.php')){
+//     include_once('about.php');
+// }
+
+
+// $name = 'kill me';
+
+// echo $name;
+
+// function age($age){
+
+//     $ety = date('Y') - $age;
+
+//     if( $ety >= 25 && $ety <= 50 ){
+//         $wow = '!!!!!!';
+//     }else{
+//         $wow = '.';
+//     }
+
+//     return "She is $ety years old $wow";
+// }
+
+
+
+// echo age(1995);
+
+// $ami[] = 'amina';
+// $ami[] = 'ayesha';
+// $ami[] = 'alysha';
+// $ami[] = 'arafat';
+// $ami[] = 'samim';
+// echo"<pre>";
+// print_r($ami);
+
+
+/*
+    isset = val set
+    empty = val set + val empty  
+*/
+
+// $amina;
+
+
+// if( isset($amina)){
+//     echo "Love you";
+// }else{
+//     echo "Miss you";
+// }
+
+// if( empty($amina)){
+//     echo "Love you";
+// }else{
+//     echo "Miss you";
+// }
+
+// echo "I love amina <hr>";
+// echo "I love amina <hr>";
+// echo "I love amina <hr>";
+// echo "I love amina <hr>";
+
+// die();
+
+// echo "I love amina <hr>";
+// echo "I love amina <hr>";
+// echo "I love amina <hr>";
+
+/**
+ * Student form isset
+ */
+
+// if( isset($_POST['submit']) ){
+//     //get all values
+//     $name = $_POST['name'];
+//     $email = $_POST['email'];
+//     $uname = $_POST['uname'];
+
+//     if( empty($name) || empty($email) || empty($uname)){
+//         echo "<span style='color : #f1c40f;background-color:#2c3e50;'>All fill are required!!";
+//     }else{
+//         echo "<span style='color : #27ae60; background-color:#f1c40f;'> Data is stable....";
+//     }
+
+
+// }
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- 
+// <form action="" method="POST" enctype="multipart/form-data">
+//     <input name="name" type="text" placeholder="Name">
+//     <input name="email" type="text" placeholder="Email">
+//     <input name="uname" type="text" placeholder="Username">
+//     <input name="submit" type="submit" Value="Add">
+// </form> -->
+
